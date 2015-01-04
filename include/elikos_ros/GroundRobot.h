f70b1d7190800fe9e101e0df4ce4336ef73ccf02
Antonio Sanniravong <antonio.sanniravong@polymtl.ca>
2015-01-04 06:45:05 -0500

    New GroundRobot class & robotsim node

---
+++ include/elikos_ros/GroundRobot.h
@@ -0,0 +1,40 @@
+/*
+ * GroundRobot.h
+ *
+ *  Created on: Jan 3, 2015
+ *      Author: Antonio Sanniravong
+ */
+
+#ifndef __GROUNDROBOT_H_
+#define __GROUNDROBOT_H_
+
+#define PI 3.14159265
+
+class GroundRobot {
+public:
+	GroundRobot(int id);
+	~GroundRobot();
+	void advance(ros::Duration cycleTime);
+	void collide();
+	void touch();
+	std::string getName();
+	tf::Transform getTransform();
+private:
+	int robotID;
+	std::string robotName;
+	bool isSpinning;
+	double x, y, z, yaw, turnAngle;
+	tf::Transform t;
+	tf::Vector3 v;
+	tf::Quaternion q;
+	ros::Time lastNoise, lastAutoReverse;
+	void reverse();
+	void autoReverse();
+	void noise();
+	void setStartingPose();
+	void setName();
+	void refreshTransform();
+	double limitTurn(double& angle, double angularSpeed, double cycleDuration);
+};
+
+#endif /* GROUNDROBOT_H_ */
---
+++ src/robotsim.cpp
@@ -0,0 +1,35 @@
+/**
+* @brief    Ground robot behavior simulation for the IARC mission 7.
+*/
+
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <elikos_ros/GroundRobot.h>
+#include <vector>
+using std::vector;
+
+int main(int argc, char** argv){
+	const int freq = 30;
+	uint32_t count = 0;
+	ros::init(argc, argv, "robotsim_tf_broadcaster");
+	ros::NodeHandle node;
+	ros::Rate r(freq);
+	std::vector<GroundRobot> robot;
+	tf::TransformBroadcaster br;
+
+	robot.reserve(10);
+	for (int i = 0; i < 10; i++){
+		robot.push_back(GroundRobot(i));
+	}
+
+	while(ros::ok()) {
+		count++;
+		for (int i = 0; i < 10; i++){
+			robot[i].advance(r.expectedCycleTime());
+			br.sendTransform(tf::StampedTransform(robot[i].getTransform(), ros::Time::now(), "world", robot[i].getName()));
+		}
+		r.sleep();
+	}
+	return 0;
+};
+
