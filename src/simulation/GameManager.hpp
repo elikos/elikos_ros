d48aa45777a574dd3c6ce2e5df77b06b91e6dbfa
Andre Nguyen <andre-phu-van.nguyen@polymtl.ca>
2015-01-13 21:47:43 -0500

    Add Game Manager

---
+++ src/simulation/GameManager.hpp
@@ -0,0 +1,13 @@
+#ifndef __GAME_MANAGER_H_
+#define __GAME_MANAGER_H_
+
+class GameManager {
+public:
+    GameManager();
+    ~GameManager();
+
+private:
+    
+};
+
+#endif /* __GAME_MANAGER_H_ */
