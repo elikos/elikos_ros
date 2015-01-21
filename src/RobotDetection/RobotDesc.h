1f9ce540f9f39dff18431b2f5ec4547ecfdca4c5
Olivier <comtois.olivier.18@gmail.com>
2015-01-20 20:57:17 -0500

    Added OpenCV librairies to the catkin ws. Added first files for the robot detection.

---
+++ src/RobotDetection/RobotDesc.h
@@ -0,0 +1,28 @@
+#include <opencv2/core/core.hpp>
+#include "string"
+
+using namespace std;
+
+class RobotDesc
+{
+public:
+    RobotDesc();
+    ~RobotDesc();
+
+    int getXPos();
+    int getYPos();
+
+    void setXPos(int pos);
+    void setYPos(int pos);
+
+    cv::Scalar getHSVmin();
+    cv::Scalar getHSVmax();
+
+    void setHSVmin(cv::Scalar scalar);
+    void setHSVmax(cv::Scalar scalar);
+
+private:
+    int xPos, yPos;
+    string type;
+    cv::Scalar HSVmin, HSVmax;
+};
