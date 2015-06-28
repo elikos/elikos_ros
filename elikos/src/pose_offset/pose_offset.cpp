595994953816f0076d724722ba87acae4058472e
Elikos default <info@elikos.polymtl.ca>
2015-06-28 19:41:39 -0400

    Fixed subscriptions

--- elikos/src/pose_offset/pose_offset.cpp
+++ elikos/src/pose_offset/pose_offset.cpp
@@ -1,24 +1,26 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Pose.h>
 #include <tf/transform_listener.h>
 
 
 bool local_pos_valid = false;
 bool vo_valid = false;
-geometry_msgs::PosePtr local_pose;
+geometry_msgs::PoseStamped local_pose;
 geometry_msgs::Pose local_pose_offset;
 geometry_msgs::PoseStamped pose_with_offset;
-geometry_msgs::PoseWithCovarianceStampedPtr vo_pose;
+geometry_msgs::PoseWithCovarianceStamped vo_pose;
 ros::Time last_vo;
 ros::Time last_local_pos;
 
 const ros::Duration timeout(0.5);
 
-void local_pos_callback(const geometry_msgs::PosePtr pose){
+void local_pos_callback(const geometry_msgs::PoseStampedConstPtr pose){
     if (!local_pos_valid) {
         ROS_WARN("Local Pose valid!");
     }
-    local_pose = pose;
+    local_pose = *pose;
     local_pos_valid = true;
     last_local_pos = ros::Time::now();
 }
@@ -28,7 +30,7 @@ void vo_pose_callback(const geometry_msgs::PoseWithCovarianceStampedPtr pose){
         ROS_WARN("VO valid! Current offset: %f, %f, %f.",
                  local_pose_offset.position.x,local_pose_offset.position.y, local_pose_offset.position.z);
     }
-    vo_pose = pose;
+    vo_pose = *pose;
     vo_valid = true;
     last_vo = ros::Time::now();
 }
@@ -36,11 +38,10 @@ void vo_pose_callback(const geometry_msgs::PoseWithCovarianceStampedPtr pose){
 int main(int argc, char **argv) {
     ros::init(argc, argv, "pose_offset");
     ros::NodeHandle nh;
-    tf::TransformListener listener;
     ros::Rate rate(100);
 
-    nh.subscribe("mavros/local_position/local", 1, &local_pos_callback);
-    nh.subscribe("svo/pose", 1, &vo_pose_callback);
+    ros::Subscriber local_pos_sub = nh.subscribe("mavros/local_position/local", 1, local_pos_callback);
+    ros::Subscriber vo_sub = nh.subscribe("svo/pose", 1, vo_pose_callback);
 
     ros::Publisher pose_pub = nh.advertise<geometry_msgs::PoseStamped>("pose_offset/pose", 1);
 
@@ -68,16 +69,17 @@ int main(int argc, char **argv) {
                 ROS_ERROR("VO timed out! Resetting offset.");
             }
             vo_valid = false;
-            local_pose_offset.position = local_pose->position;
-            local_pose_offset.orientation = local_pose->orientation;
+            local_pose_offset.position = local_pose.pose.position;
+            local_pose_offset.orientation = local_pose.pose.orientation;
         }
 
         if(local_pos_valid && vo_valid){
-            pose_with_offset.pose.position.x = local_pose_offset.position.x + vo_pose->pose.pose.position.x;
-            pose_with_offset.pose.position.y = local_pose_offset.position.y + vo_pose->pose.pose.position.y;
-            pose_with_offset.pose.position.z = local_pose_offset.position.z + vo_pose->pose.pose.position.z;
-            pose_with_offset.pose.orientation = vo_pose->pose.pose.orientation;
+            pose_with_offset.pose.position.x = local_pose_offset.position.x + vo_pose.pose.pose.position.x;
+            pose_with_offset.pose.position.y = local_pose_offset.position.y + vo_pose.pose.pose.position.y;
+            pose_with_offset.pose.position.z = local_pose_offset.position.z + vo_pose.pose.pose.position.z;
+            pose_with_offset.pose.orientation = vo_pose.pose.pose.orientation;
             pose_with_offset.header.stamp = ros::Time::now();
+            pose_with_offset.header.frame_id = "local_origin";
 
             pose_pub.publish(pose_with_offset);
         }
