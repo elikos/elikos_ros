f62afc26842391a1fbc560d9118a4e05138ea1f0
Olivier St-Amour <olivierstamour0952@gmail.com>
2016-05-08 21:00:04 -0400

    Le ai retrouve correctement les tf.

---
+++ src/ai/Robot/ObstacleRobot.h
@@ -0,0 +1,16 @@
+#ifndef AI_OBSTACLEROBOT_H
+#define AI_OBSTACLEROBOT_H
+
+#include "Robot.h"
+
+namespace ai
+{
+
+class ObstacleRobot : public Robot
+{
+public:
+    ObstacleRobot()=default;
+    ~ObstacleRobot()=default;
+};
+
+}
---
+++ src/ai/Robot/QuadRobot.h
@@ -0,0 +1,16 @@
+#ifndef AI_QUADROBOT_H
+#define AI_QUADROBOT_H
+
+#include "Robot.h"
+
+namespace ai
+{
+
+class QuadRobot : public Robot
+{
+public:
+    QuadRobot()=default;
+    ~QuadRobot()=default;
+};
+
+}
