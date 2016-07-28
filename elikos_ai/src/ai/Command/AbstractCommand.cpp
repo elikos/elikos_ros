a362c62c24f27c9c1bbd60c465f21bc997af3ca8
Elikos <elikos.bot@gmail.com>
2016-07-28 19:00:54 -0400

    Fix de la verification de la position dans l AI pour permettre au drone de savoir qu il a  atteint une cible au sol.

--- elikos_ai/src/ai/Command/AbstractCommand.cpp
+++ elikos_ai/src/ai/Command/AbstractCommand.cpp
@@ -14,8 +14,17 @@ AbstractCommand::~AbstractCommand()
 
 bool AbstractCommand::hasReachedDestination(const tf::Vector3& currentPosition, const tf::Vector3& destination)
 {
-    double distance = tf::tfDistance(currentPosition, destination);
-    
+    double distance;
+    if(destination.getZ() == -1.0)
+    {
+      tf::Vector3 groundPosition = destination;
+      groundPosition.setZ(0);
+      distance = tf::tfDistance(currentPosition, groundPosition);
+    }
+    else
+    {
+      distance = tf::tfDistance(currentPosition, destination);
+    }
     return std::abs(distance) < 0.4;
 }
 
