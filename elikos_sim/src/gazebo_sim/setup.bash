e95b69325ab3bc83aebdf470383766968d9c916f
Olivier St-Amour <olivierstamour0952@gmail.com>
2017-01-03 14:09:32 -0500

    Launch file for gazebo sim created

--- elikos_sim/src/gazebo_sim/setup.bash
+++ elikos_sim/src/gazebo_sim/setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-source Firmware/Tools/setup_gazebo.bash $(pwd) $(pwd)/Firmware/build_posix_default
+source Firmware/Tools/setup_gazebo.bash $(pwd)/Firmware $(pwd)/Firmware/build_posix_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Firmware/Tools/sitl_gazebo
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(pwd)/elikos_gazebo_models/models
 
