00869b6a6de1c1cf47afb35e69fb21e96adcdba1
christophebedard <chrisvolkoff@gmail.com>
2017-11-27 14:45:07 -0500

    Support more than 4 obstacle robots

--- elikos_sim/src/rviz_sim/roomba_sim.cpp
+++ elikos_sim/src/rviz_sim/roomba_sim.cpp
@@ -12,12 +12,7 @@ static const std::string ARENA_SQUARE_MODEL = "package://elikos_roomba/models/ar
 static const double TARGET_ROBOT_RADIUS = 1.0;
 static const double OBSTACLE_ROBOT_RADIUS = 5.0;
 
-//visualization_msgs::MarkerArray markerarray_msg;
-//ros::Publisher arenasquares_pub;
-
 std::vector<Robot*> robots;
-
-int nbTargetRobots, nbObstacleRobots;
 
 
 int main(int argc, char** argv){
@@ -25,6 +20,7 @@ int main(int argc, char** argv){
     ros::NodeHandle n;
 
     ros::NodeHandle n_p("~");
+    int nbTargetRobots, nbObstacleRobots;
     n_p.getParam("/target_robot_count", nbTargetRobots);
     n_p.getParam("/obstacle_robot_count", nbObstacleRobots);
 
@@ -43,13 +39,14 @@ int main(int argc, char** argv){
         double pos_x = OBSTACLE_ROBOT_RADIUS * cos(i * (360 / nbObstacleRobots) * DEG_TO_RAD);
         double pos_y = OBSTACLE_ROBOT_RADIUS * sin(i * (360 / nbObstacleRobots) * DEG_TO_RAD);
         double yaw = (-90.0 + (i * (360 / nbObstacleRobots))) * DEG_TO_RAD;
-        robots.push_back(new ObstacleRobot(n, i+1, tf::Vector3(pos_x, pos_y, 0.0), yaw, heights[i]));
+        std::string height = heights[i%4];
+        robots.push_back(new ObstacleRobot(n, i+1, tf::Vector3(pos_x, pos_y, 0.0), yaw, height));
     }
 
     ros::Rate rate(LOOP_RATE_SIM);
     while (ros::ok())
     {
-        for (auto const& robot : robots) {
+        for (auto& robot : robots) {
             robot->update();
         }
 
